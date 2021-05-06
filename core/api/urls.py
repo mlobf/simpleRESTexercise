3ab0bbe83318af5c2aa96e419d385f7326d2911a
mlobf <marcoslemeborbafilho@gmail.com>
2021-05-06 03:40:50 -0300

    Add Filter, Todo list

--- core/api/urls.py
+++ core/api/urls.py
@@ -2,14 +2,30 @@ from django.urls import path
 from core.api.views import (
     ImovelDetailAPIview,
     ImovelListCreateAPIView,
+    ImovelListAPIView,
     ImobiliariaDetailAPIview,
     ImobiliariaListCreateAPIView,
+    ImobiliariaListAPIView,
 )
 
 urlpatterns = [
+    # -------------------------------------------------------------------------------------------------#
     path("imovel/<int:pk>/", ImovelDetailAPIview.as_view(), name="imovel-detail"),
     path("imovel/", ImovelListCreateAPIView.as_view(), name="imovel-list"),
-
-    path("imobiliaria/<int:pk>/", ImobiliariaDetailAPIview.as_view(), name="imobiliaria-detail"),
-    path("imobiliaria/", ImobiliariaListCreateAPIView.as_view(), name="imobiliaria-list"),
+    path("imovel/search", ImovelListAPIView.as_view(), name="imovel-search"),
+    # -------------------------------------------------------------------------------------------------#
+    path(
+        "imobiliaria/<int:pk>/",
+        ImobiliariaDetailAPIview.as_view(),
+        name="imobiliaria-detail",
+    ),
+    path(
+        "imobiliaria/", ImobiliariaListCreateAPIView.as_view(), name="imobiliaria-list"
+    ),
+    path(
+        "imobiliaria/search",
+        ImobiliariaListAPIView.as_view(),
+        name="imobiliaria-search",
+    ),
+    # -------------------------------------------------------------------------------------------------#
 ]
--- core/api/views.py
+++ core/api/views.py
@@ -1,8 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from core.models import Imovel, Imobiliaria
 from core.api.serializers import ImovelSerializer, ImobiliariaSerializer
 
-
+# ------------------      Imovel           -------------------------------------#
 class ImovelListCreateAPIView(generics.ListCreateAPIView):
     queryset = Imovel.objects.all().order_by("-id")
     serializer_class = ImovelSerializer
@@ -15,6 +15,14 @@ class ImovelDetailAPIview(generics.RetrieveUpdateDestroyAPIView):
     # permission_classes = [IsAdminUserOrReadOnly]
 
 
+class ImovelListAPIView(generics.ListAPIView):
+    queryset = Imovel.objects.all()
+    serializer_class = ImovelSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["nome"]
+
+
+# -------------------------  Imobiliaria    ------------------------------------#
 class ImobiliariaListCreateAPIView(generics.ListCreateAPIView):
     queryset = Imobiliaria.objects.all().order_by("-id")
     serializer_class = ImobiliariaSerializer
@@ -25,3 +33,11 @@ class ImobiliariaDetailAPIview(generics.RetrieveUpdateDestroyAPIView):
     queryset = Imobiliaria.objects.all()
     serializer_class = ImobiliariaSerializer
     # permission_classes = [IsAdminUserOrReadOnly]
+
+class ImobiliariaListAPIView(generics.ListAPIView):
+    queryset = Imobiliaria.objects.all()
+    serializer_class = ImobiliariaSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["nome"]
+
+# ---------------------------------------------------------------------------#
