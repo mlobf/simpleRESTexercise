4aa7eb20b6cc7ba288931e6c46cb6ea891c00951
mlobf <marcoslemeborbafilho@gmail.com>
2021-05-10 00:38:11 -0300

    Add Comments on Views and Serializers

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,16 +1,35 @@
 from rest_framework import serializers
 from core.models import Imovel, Imobiliaria
 
+"""
+    Foi utilizado uma abstração de alto nivel
+    do Django Rest Framework chamada serializers.ModelSerializer.
+    
+    => https://www.django-rest-framework.org/api-guide/serializers/#modelserializer
 
+    Esta tem por objetivo, tendo por base uma Tabela/Modelo, realizar
+    processo de "transformação" de determinados campos da Tabela em JSON.
+
+    Atraves da Classe Meta é possivel determinal:
+        ->model = O nome da Tabela.
+        ->fields = Os campos a serem transformados em JSON da respectiva Tabela.
+
+"""
+
+
+# Esta Classe realiza o processo de serializacao da tabela Imobiliaria.
 class ImobiliariaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Imobiliaria
+        # Seleciona os campos a ser serializados da tabela Imobiliaria.
         fields = ["id", "nome", "endereço"]
 
 
+# Esta Classe realiza o processo de serializacao da tabela Imovel.
 class ImovelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Imovel
+        # Seleciona os campos a ser serializados da tabela Imovel.
         fields = [
             "id",
             "nome",
--- core/api/views.py
+++ core/api/views.py
@@ -5,9 +5,26 @@ from core.models import Imovel, Imobiliaria
 from core.api.serializers import ImovelSerializer, ImobiliariaSerializer
 from django.http import JsonResponse, HttpResponse
 
-# ------------------      Imovel           -------------------------------------#
+"""
+    Foi utilizado uma abstração de alto nivel
+    do Django Rest Framework chamada generic.APIViews.
+    
+    => https://www.django-rest-framework.org/api-guide/generic-views/
+
+    Esta possui metodos que representam das entidades as seguintes operações:
+
+        -> Listagem.
+        -> Criação.
+        -> Detalhamento. 
+        -> Atualização.
+        -> Destruição.
+
+"""
+
+
+# --------------------------  Imovel    -------------------------------------#
 class ImovelListAPIView(generics.ListAPIView):
-    queryset = Imovel.objects.all()
+    queryset = Imovel.objects.all().order_by("nome")
     serializer_class = ImovelSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ["nome"]
@@ -21,8 +38,6 @@ class ImovelCreateAPIView(generics.CreateAPIView):
 class ImovelDetailAPIView(generics.RetrieveAPIView):
     queryset = Imovel.objects.all()
     serializer_class = ImovelSerializer
-    # search_fields = ["nome"]
-    # permission_classes = [IsAdminUserOrReadOnly]
 
 
 class ImovelUpdateAPIView(generics.UpdateAPIView):
@@ -35,9 +50,9 @@ class ImovelDestroyAPIView(generics.DestroyAPIView):
     serializer_class = ImovelSerializer
 
 
-# -------------------------  Imobiliaria    ------------------------------------#
+# -------------------------  Imobiliaria    ---------------------------------#
 class ImobiliariaListAPIView(generics.ListAPIView):
-    queryset = Imobiliaria.objects.all().order_by("-id")
+    queryset = Imobiliaria.objects.all().order_by("nome")
     serializer_class = ImobiliariaSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ["nome"]
@@ -46,20 +61,21 @@ class ImobiliariaListAPIView(generics.ListAPIView):
 class ImobiliariaCreateAPIView(generics.CreateAPIView):
     queryset = Imobiliaria.objects.all()
     serializer_class = ImobiliariaSerializer
-    # permission_classes = [IsAdminUserOrReadOnly]
 
 
 class ImobiliariaDetailAPIView(generics.RetrieveAPIView):
     queryset = Imobiliaria.objects.all()
     serializer_class = ImobiliariaSerializer
-    # search_fields = ["nome"]
-    # permission_classes = [IsAdminUserOrReadOnly]
+
 
 class ImobiliariaUpdateAPIView(generics.UpdateAPIView):
     queryset = Imobiliaria.objects.all().order_by("-id")
     serializer_class = ImobiliariaSerializer
 
+
 class ImobiliariaDestroyAPIView(generics.DestroyAPIView):
     queryset = Imobiliaria.objects.all().order_by("-id")
     serializer_class = ImobiliariaSerializer
-# ---------------------------------------------------------------------------#
+
+
+# ----------------------------------------------------------------------------#
