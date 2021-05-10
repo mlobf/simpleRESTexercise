6454ad4704bcabb6899f4c9d18c6155294cdd805
mlobf <marcoslemeborbafilho@gmail.com>
2021-05-10 02:44:52 -0300

    pre postgres

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# TestDev
+
+Criar o banco de dados resale no Postgres usando Linux Ubuntu.
+
+postgres@seucomputador:/home/seuusuario$ psql
+psql (12.6 (Ubuntu 12.6-0ubuntu0.20.04.1))
+
+postgres=# create database resale;
+CREATE DATABASE
+postgres=# \c resale
+You are now connected to database "resale" as user "postgres".
+resale=# create user resale with password 'resale';
+CREATE ROLE
+resale=# grant all privileges on database resale to resale;
+GRANT
+resale=# grant ALL on ALL tables in schema public to resale;
+GRANT
+resale=# grant all on ALL  sequences in schema public to resale;
+GRANT
+resale=# grant ALL on ALL functions in schema public to resale;
+GRANT
+resale=# create extension unaccent;
+CREATE EXTENSION
+resale=#\q
+
--- Resale/settings.py
+++ Resale/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,12 +76,31 @@ WSGI_APPLICATION = "Resale.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+'''
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASS"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
+    }
+}
+
+
+
+
+
+
+
 
 
 # Password validation
